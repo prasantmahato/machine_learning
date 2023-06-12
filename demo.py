ac17da53b95cdc94e92c868427a1ce51b5e6889e
Prasant Kumar Mahato <prasantmahato33@gmail.com>
2023-06-12 10:55:47 +0530

    Basics operations on numpy

--- demo.py
+++ demo.py
@@ -1,7 +1,39 @@
-import numpy
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = numpy.random.uniform(0.0, 5.0, 250)
+# plt.style.use('_mpl-gallery')
 
-plt.hist(x, 5)
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+def fx(x,a, b,c):
+    return a*x*x + b*x + c
+
+def gx(x,c):
+    return c*x*x
+
+# make data
+# 3n^2 - 100n + 6
+lim = 200
+
+# fx
+a=3
+b=(-100)
+c=6
+
+# gx
+c1=2
+
+y1 = []
+y2 = []
+for i in range(0, lim):
+    y1.append(fx(i, a, b,c))
+    y2.append(gx(i,c1))
+    
+x1=[x for x in range(0, lim)]
+x2=x1
+
+# plot
+plt.plot(x1,y1)
+plt.plot(x2,y2)
 plt.show()
